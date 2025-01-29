09121345bd9af1455263be05e74d72d00d577a28
Cubba2412 <thomasbf@live.dk>
2025-01-29 23:26:18 +0100

    Add RSVP. Change poem spacing

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -16,6 +16,13 @@ const MainHero = () => {
         <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
           {mainHero.description}
         </p>
+        <br />
+        <h1 className="text-xl tracking-tight font-extrabold text-gray-900 sm:text-xl md:text-2xl">
+          <span className={`block text-gray-900 xl:inline`}>
+            {mainHero.primaryAction.text}
+          </span>
+        </h1>
+
         {/* <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div className="rounded-md shadow">
             <a
@@ -24,16 +31,16 @@ const MainHero = () => {
             >
               {mainHero.primaryAction.text}
             </a>
-          </div>
-          <div className="mt-3 sm:mt-0 sm:ml-3">
+          </div> 
+           <div className="mt-3 sm:mt-0 sm:ml-3">
             <a
               href={mainHero.secondaryAction.href}
               className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
             >
               {mainHero.secondaryAction.text}
             </a>
-          </div>
-        </div> */}
+          </div> 
+        </div>  */}
       </div>
     </main>
   );
